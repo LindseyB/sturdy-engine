8708c6629d8f329265dfd98853e4bd6b6bb8c5c6
Lindsey Bieda <hazardous.narayan@gmail.com>
2022-01-01 17:29:56 +0100

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,19 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 13,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {},
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
-};
+}
