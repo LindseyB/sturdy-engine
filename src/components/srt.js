458c47a4760da173fbb6722cfa2b28aa2e56ac4d
Lindsey Bieda <hazardous.narayan@gmail.com>
2022-01-15 17:31:50 -0500

    Parse and display SRT

--- src/components/srt.js
+++ src/components/srt.js
@@ -1,6 +1,7 @@
 import '../../scss/main.scss'
 
 import React from 'react'
+import srtParser2 from 'srt-parser-2'
 import { Panel } from 'react-bulma-components'
 import PropTypes from 'prop-types'
 
@@ -8,21 +9,23 @@ export class Srt extends React.Component {
   constructor(props) {
     super(props)
 
-    console.log(this.props.srt)
+    const parser = new srtParser2()
+    let subtitles = parser.fromSrt(this.props.srt)
+
+    console.log(subtitles)
 
     this.state = {
-      subititles: [],
+      subtitles: subtitles,
     }
-  }
-
-  useEffect() {
-    console.log(this.props.srt)
   }
 
   render() {
     return (
       <Panel>
         <Panel.Header>Select Subtitles to Generate GIFs</Panel.Header>
+        {this.state.subtitles.map((subtitle) => {
+          return <Panel.Block key={subtitle.id}>{subtitle.text}</Panel.Block>
+        })}
       </Panel>
     )
   }
